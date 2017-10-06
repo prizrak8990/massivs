8e06df562d54b44d5f82b2604b0412b0c6011804
prizrak8990 <32384718+prizrak8990@users.noreply.github.com>
2017-10-06 11:27:38 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,11 +7,16 @@ cout<<"input:";
 cin >> n >> m;
 int a[n][m], b[n][m], c[n][m];
  for(i=0; i<n; i++)
-  for (j=0; j<m; j++)
-  {
-   cin >> a[i][j] >> b[i][j];
-   c[i][j]=a[i][j]+b[i][j];
-  }
+   for (j=0; j<m; j++)
+   {
+    cin >> a[i][j];
+   }
+ for(i=0; i<n; i++)
+   for (j=0; j<m; j++)
+   {
+    cin >> b[i][j];
+    c[i][j]=a[i][j]+b[i][j];
+   }
  for(i=0; i<n; i++)
  {
   for (j=0; j<m; j++)
